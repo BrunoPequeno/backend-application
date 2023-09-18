db6943eb01d546bb1cde84af476c32e5ebf62dc8
Bruno Pequeno <bru.pequeno@gmail.com>
2023-09-17 22:37:51 -0300

    Updating Maven

--- backend-application/src/main/java/com/unlockway/backendapplication/BackendApplication.java
+++ backend-application/src/main/java/com/unlockway/backendapplication/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.unlockway.backendapplication;
 
+import com.google.firebase.FirebaseApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
